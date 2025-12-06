c199d19a0b1ff55e9d4daedb2999ea38ba699014
Nguyễn Thế Hiếu <153330033+thehieu03@users.noreply.github.com>
2025-12-06 22:01:05 +0700

    page user

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -4,7 +4,7 @@ const UserDetails = async ({params}: { params: Promise<{ id: string }> }) => {
     const {id} = await params;
     return (
         <div>
-            <h1>Showing detials of User by id {id}</h1>
+            <h1>Showing details of User by id {id}</h1>
         </div>
     );
 };
